d2de23369aeafcc15c401c67bc4db65268bd54d6
wy327 <495139290@qq.com>
2021-10-15 15:28:12 +0800

    提交

---
+++ src/com/test/fx/MainStart.java
@@ -0,0 +1,17 @@
+package com.test.fx;
+
+import com.test.fx.view.app.test1.Test;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainStart extends Application {
+
+    public static void main(String[] args){
+        launch(Test.class,args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/com/test/fx/view/app/test1/Test.java
@@ -0,0 +1,15 @@
+package com.test.fx.view.app.test1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("fxDemo");
+        primaryStage.setHeight(600);
+        primaryStage.setWidth(600);
+        primaryStage.show();
+    }
+}
